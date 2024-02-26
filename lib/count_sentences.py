bf8f8717bd308fcfbac2f99fd46d5f6f0d006a1e
Matthew Kennedy <mkennedy.akgrown@gmail.com>
2024-02-26 11:06:26 -0600

    Completed Lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  def __init__(self, value=""):
+    self.value = value
+  
+  @property
+  def value(self):
+    return self._value
+  
+  @value.setter
+  def value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+    
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+  
+  def is_exclamation(self):
+    return self.value.endswith("!")
+  
+  def count_sentences(self):
+    return self.value.count(". ") + self.value.count("? ") + self.value.count("! ") + (1 
+                                                                                       if self.value.endswith(".")
+                                                                                        or self.value.endswith("?")
+                                                                                        or self.value.endswith("!")
+                                                                                        else 0)
